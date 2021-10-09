0344c5ecdb7e576d51db4f94451054a08e91b1c2
Evan <venter305@gmail.com>
2021-10-08 23:40:07 -0400

    Update

--- Window.h
+++ Window.h
@@ -17,6 +17,9 @@ class Window {
 
     Window(int w,int h,std::string name):width(w),height(h),title(name),baseHeight(h),baseWidth(w){
       window = glfwCreateWindow(width,height,title.c_str(),NULL, NULL);
+      const char* data;
+      int error = glfwGetError(&data);
+      std::cout << data << std::endl;
     };
 
     void Init(int w,int h,std::string name){
